a9f440cf5f50c6aac8785a8bfe7b76129e212054
Salztech1 <salomenjinda0@gmail.com>
2023-11-19 13:08:41 -0800

    updated files

--- monty.c
+++ monty.c
@@ -1,27 +1,29 @@
 #include "monty.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-char **op_toks = NULL;
 
 /**
- * main - Function to the entry point for Monty Interpreter function
- * @argc: input param (the count of arguments passed to the program)
- * @argv: input param (pointer to an array of char pointers to arguments)
+ * main - opens a monty script file for parsing
+ * @argc: count of arguments
+ * @argv: array of arguments
  *
- * Return: (EXIT_SUCCESS) on success and (EXIT_FAILURE) on error
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	FILE *script_fd = NULL;
-	int exit_code = EXIT_SUCCESS;
+	FILE *file;
 
 	if (argc != 2)
-		return (sys_err_usage());
-	script_fd = fopen(argv[1], "r");
-	if (script_fd == NULL)
-		return (sys_err_fopen(argv[1]));
-	fclose(script_fd);
-	return (exit_code);
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	parsefile(file);
+	return (EXIT_SUCCESS);
 }
